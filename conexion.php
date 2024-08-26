423b579cf5aa6aa98d2391501cb26e73c87ed497
Alm4en0 <144942301+Alm4en0@users.noreply.github.com>
2024-08-26 17:25:11 -0500

    updateDB

--- conexion.php
+++ conexion.php
@@ -1,11 +1,11 @@
 <?php
 	function conexion(){
 
-	$host = "host=viaduct.proxy.rlwy.net";
-	$port = "port=26862";
-	$dbname = "dbname=railway";
-	$user = "user=postgres";
-	$password = "password=SIIZpYZuHrHzKtzfUhAFNdDKKPEACBSJ";
+	$host = "host=dpg-cr6g04lumphs73b4hod0-a";
+	$port = "port=5432";
+	$dbname = "dbname=dbclase_u9wu";
+	$user = "user=dbclase_u9wu_user";
+	$password = "password=qf9rFKDsMjhSaHSEMZYaMHpNYIygVhzm";
 
 	$db = pg_connect("$host $port $dbname $user $password");
 
