3c3ac1bf798e707fc19eb2340bdf9b8ef1cf1e71
Alm4en0 <144942301+Alm4en0@users.noreply.github.com>
2024-08-31 00:31:19 -0500

    Corrigiendo conexion.php

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 	function conexion(){
 
-	$host = "host=dpg-cr6g04lumphs73b4hod0-a";
+	$host = "host=dpg-cr6g04lumphs73b4hod0-a.oregon-postgres.render.com";
 	$port = "port=5432";
 	$dbname = "dbname=dbclase_u9wu";
 	$user = "user=dbclase_u9wu_user";
